54dc86016c1088aa1a7e4102a018e3f81ea845ef
Mariano <mariano.palliser@gmail.com>
2017-02-04 10:37:19 +0100

    first commit

---
+++ js/js.js
@@ -0,0 +1,17 @@
+window.onload=function() {
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function() {
+	 if (this.readyState == 4 && this.status == 200) {
+	  gestionarXml(this);
+	 }
+
+	};
+	xhttp.open("GET", "xml/preguntas.xml", true);
+	xhttp.send();
+
+	function gestionarXml(dadesXml){
+	var xmlDoc = dadesXml.responseXML;
+	document.getElementById("rellenar").innerHTML = xmlDoc.getElementById("primeraPregunta").getElementsByTagName("title")[0].childNodes[0].nodeValue;
+	};
+}
