22478eafc67838809f95884c05bf25f0ae8295f3
Mariano <mariano.palliser@gmail.com>
2017-02-04 16:15:10 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,5 @@
 window.onload=function() {
 	var xhttp = new XMLHttpRequest();
-
 	xhttp.onreadystatechange = function() {
 	 if (this.readyState == 4 && this.status == 200) {
 	  gestionarXml(this);
@@ -14,6 +13,5 @@ window.onload=function() {
 	var xmlDoc = dadesXml.responseXML;
 	document.getElementById("rellenar1").innerHTML = xmlDoc.getElementById("quest001").getElementsByTagName("title")[0].childNodes[0].nodeValue;
 	document.getElementById("rellenar2").innerHTML = xmlDoc.getElementById("quest002").getElementsByTagName("title")[0].childNodes[0].nodeValue;
-
 	};
 }
