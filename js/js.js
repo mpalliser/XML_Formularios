d87d1e8fbea9c630459e370e08ec6d0a75c244c6
Mariano <mariano.palliser@gmail.com>
2017-02-05 11:49:08 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -8,10 +8,18 @@ window.onload=function() {
 	};
 	xhttp.open("GET", "xml/preguntas.xml", true);
 	xhttp.send();
-
-	function gestionarXml(dadesXml){
-	var xmlDoc = dadesXml.responseXML;
-	document.getElementById("rellenar1").innerHTML = xmlDoc.getElementById("quest001").getElementsByTagName("title")[0].childNodes[0].nodeValue;
-	document.getElementById("rellenar2").innerHTML = xmlDoc.getElementById("quest002").getElementsByTagName("title")[0].childNodes[0].nodeValue;
-	};
 }
+function gestionarXml(dadesXml){
+ var xmlDoc = dadesXml.responseXML;
+ var pregunta1=xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+ var opcionesSelect = [];
+ var nopt = xmlDoc.getElementById("quest001").getElementsByTagName('option').length;
+  for (i = 0; i < nopt; i++) { 
+   opcionesSelect[i] = xmlDoc.getElementById("quest001").getElementsByTagName('option')[i].childNodes[0].nodeValue;
+ }
+ ponerDatosSelectHtml(pregunta1,opcionesSelect);
+ respuestaSelect=parseInt(xmlDoc.getElementsByTagName("answer")[0].childNodes[0].nodeValue);
+
+};
+
+
